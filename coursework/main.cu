03204282df5b2d38092d1e8f57eed3fcd2feb045
gedenteen <gedenteen@gmail.com>
2021-05-24 09:41:27 +0700

    Add coursework ver 2

--- coursework/main.cu
+++ coursework/main.cu
@@ -1,6 +1,6 @@
 #include "header.h"
 
-int main() {	
+int main(int argc, char *argv[]) {	
 	/// информация об используемом устройстве:
 	cudaDeviceProp deviceProp;
 	cudaGetDeviceProperties(&deviceProp, 0);
@@ -15,7 +15,29 @@ int main() {
 	long int arr_size = 1 << 25; //максимальный размер массивов
 	float alpha = 2.5f; //коэффициент умножения для массива X
 	int iterations = 6; //сколько раз запускать SAXPY
-	int check_arrays = 0; //
+	int check_arrays = 0; //сколько элементов массива вывести на экран
+	
+	/// пользователь может изменить эти параметры, задав их при запуске:
+	if (argc >= 2) {
+		int degree = atoll(argv[1]); //преобразование строки в long int
+		if (degree > 0)
+			arr_size = 1 << degree;
+		else 
+			fprintf(stderr, "error, degree should be > 0\n");
+	}
+	if (argc >= 3) {
+		alpha = atof(argv[2]); //преобразование строки в float
+	}
+	if (argc >= 4) {
+		int tmp = atoll(argv[3]);
+		if (tmp > 0)
+			iterations = tmp;
+		else
+			fprintf(stderr, "error, iterations should be > 0\n");
+	}
+	if (argc >= 5) {
+		check_arrays = atoll(argv[4]);
+	}
 	
 	/// посчитать среднее время и вывести результаты:
 	printf("   SAXPY\n");
